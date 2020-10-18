e937e04b645a6c752bb974160001279785e8a88a
Lucy Phipps <landfillbaby@outlook.com>
2020-10-18 06:16:59 +0100

    reproducible Windows builds, hopefully

--- compile_msvc.bat
+++ compile_msvc.bat
@@ -1,11 +1,11 @@
 pushd "%~dp0"
 copy libpng\scripts\pnglibconf.h.prebuilt libpng\pnglibconf.h
 del libpng\pngtest.c
-cl.exe /nologo /std:c11 /W2 /O2 /Ob3 /GL /Gw /Qpar /DNDEBUG ^
+cl.exe /nologo /std:c11 /W2 /Brepro /O2 /Ob3 /GL /Gw /Qpar /DNDEBUG ^
     /Ilibpng /Izlib /Ilibwebp /Ilibwebp\src ^
     %* png2webp.c libpng\png*.c zlib\*.c ^
     libwebp\src\enc\*.c libwebp\src\utils\*.c libwebp\src\dsp\*.c
-cl.exe /nologo /std:c11 /W2 /O2 /Ob3 /GL /Gw /Qpar /DNDEBUG ^
+cl.exe /nologo /std:c11 /W2 /Brepro /O2 /Ob3 /GL /Gw /Qpar /DNDEBUG ^
     /Ilibpng /Izlib /Ilibwebp /Ilibwebp\src ^
     %* webp2png.c libpng\png*.c zlib\*.c ^
     libwebp\src\dec\*.c libwebp\src\utils\*.c libwebp\src\dsp\*.c
